397c22a8bd560cc9abcba436a2a5501b883d5a82
Jakub Bajer <kuba@bajer.net>
2024-05-25 01:37:58 +0200

    add transpartent bg

--- init.lua
+++ init.lua
@@ -796,6 +796,7 @@ require('lazy').setup({
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
       vim.g.everforest_enable_italic = true
+      vim.g.everforest_transparent_background = 2
       vim.cmd.colorscheme 'everforest'
     end,
   },
