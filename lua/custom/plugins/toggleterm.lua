1082f475683614da3c45c35190bdb4db03f8c7e0
Jakub Bajer <kuba@bajer.net>
2024-06-10 23:28:13 +0200

    add some plugins

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = true,
+    keys = {
+      { '<leader>tt', '<cmd>ToggleTerm size=15 direction=horizontal<cr>', desc = 'Toggle terminal horizontally' },
+    },
+  },
+}
